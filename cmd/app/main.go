a8f96c5f38fc083d43803b556196d4767216a825
mihazzzold <m.dokuchaev73@gmail.com>
2022-07-19 22:31:17 +0300

    feat: refactore

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"log"
 	"upload-big-file-to-elma/config"
+	"upload-big-file-to-elma/internal/app"
 )
 
 func main() {
 	// Configuration
-	config, err := config.NewConfig()
+	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
 
 	// Run
-	app.Run(config)
+	app.Run(cfg)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,11 @@
 package app
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
+	"syscall"
 	"upload-big-file-to-elma/config"
 	v1 "upload-big-file-to-elma/internal/controllers/http/v1"
 	"upload-big-file-to-elma/internal/usecase"
@@ -22,4 +26,19 @@ func Run(cfg *config.Config) {
 	v1.NewRouter(handler, l, bigfileUseCase)
 	httpServer := httpserver.New(handler, httpserver.Port(cfg.HTTP.Port))
 
+	// Waiting signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		l.Info("app - Run - signal: " + s.String())
+	case err := <-httpServer.Notify():
+		l.Error(fmt.Errorf("app - Run -- httpServer.Notify: %w", err))
+	}
+
+	// Shutdown
+	if err := httpServer.Shutdown(); err != nil {
+		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
+	}
 }
